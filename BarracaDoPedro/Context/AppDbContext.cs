8005e6e2d439bd5e8abeca1d2dff8a9fa7ee7d0e
rjsilvaoficial <rjsilvaoficial@gmail.com>
2021-10-17 14:30:59 -0300

    Iniciado projeto com inclusao de classe produto, restricao de campos com data annotations e criacao de classe contexto e disponibilizacao da mesma nos services do startup

---
+++ BarracaDoPedro/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BarracaDoPedro.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BarracaDoPedro.Context
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Produto> Produto { get; set; }
+
+    }
+}
---
+++ BarracaDoPedro/Models/Produto.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace BarracaDoPedro.Models
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        [StringLength(100)]
+        public string Nome { get; set; }
+        [StringLength(100)]
+        public string Descricao { get; set; }
+        [StringLength(200)]
+        public string Imagem { get; set; }
+        public bool Estoque { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
